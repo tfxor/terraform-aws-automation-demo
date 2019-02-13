ebee7e2880887abef36f61bc7b6a2449d86b3eb1
root <root@DESKTOP-ECSQPB1.localdomain>
2019-02-13 16:26:57 +0100

    convert HCL to THub config

--- iam_group/main.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_iam_group" "iam_group" {
-  name     = "${var.iam_group_name}"
-  path     = "${var.iam_group_path}"
-}
--- iam_user_group_membership/output.tf
+++
@@ -1,9 +0,0 @@
-# Define list of variables to be output
-
-output "user" {
-  value = "${aws_iam_user_group_membership.iam_user_group_membership.user}"
-}
-
-output "groups" {
-  value = "${aws_iam_user_group_membership.iam_user_group_membership.groups}"
-}
