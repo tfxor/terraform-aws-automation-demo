3803eae5f1114902326c148db93487a7c0033a33
Eugene I <eugene@mitocgroup.com>
2020-01-17 14:33:58 -0500

    minor improvement

--- demo.js
+++ demo.js
@@ -1,8 +1,7 @@
 exports.handler = async (event) => {
-    // TODO implement
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!'),
-    };
-    return response;
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify('Hello from Lambda!'),
+  };
+  return response;
 };
